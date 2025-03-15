b096f91b2057f039cb664b75999de6f4f4dcf89d
Md Rasel <mdrasel551219@gmail.com>
2025-03-15 11:30:40 +0600

    add:blog page and compare page added

--- app/components/BlogPage/Blogs.js
+++ app/components/BlogPage/Blogs.js
@@ -23,7 +23,7 @@ export default function Blogs() {
         </div>
       </div>
       {/* blogs */}
-      <div className="container mx-auto px-4 py-8 mb-24">
+      <div className="container mx-auto px-4  mb-24">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <div className=" flex flex-col items-start gap-6 mb-16">
             <div className="w-full">
