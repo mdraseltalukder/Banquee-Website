97195a22a9e04aeecc054bd524a1cd1bba9bbfb1
Md Rasel <mdrasel551219@gmail.com>
2025-03-15 12:57:24 +0600

    add:blog page and compare page added

--- app/components/Home/Hero.js
+++ app/components/Home/Hero.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 import { IoMdArrowForward } from "react-icons/io";
 export default function Hero() {
   return (
-    <div className="flex flex-col gap-3 items-center pt-[128px] bg-[#e8f2ee]">
+    <div className="flex flex-col px-3 lg:px-0 gap-3 items-center text-center justify-center pt-[128px] bg-[#e8f2ee]">
       <h1>All in one card.</h1>
       <h4>
-        Senectus et netus et malesuada fames ac turpis. Sagittis vitae et leo
-        duis ut diam.
+        Senectus et netus et malesuada fames ac turpis. <br /> Sagittis vitae et
+        leo duis ut diam.
       </h4>
       <div className="flex items-center gap-6 mt-6 mb-[152px]">
         <button className="px-4 py-2 text-white bg-[#5BB5A2] rounded-lg">
